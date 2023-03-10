325035ba2559179d55af0e15396cd652e84e2de8
AlexvanRooij <alex.rooij@icloud.com>
2023-03-10 14:04:56 +0100

    Comments added

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+// Hier maak de de burgers array aan die er voor zorgt dat die ik kan gebruiken later
 const burgers = [
   { name: 'Big King XL',
     price: 5.95,
@@ -26,19 +27,22 @@ const burgers = [
 ]
 
 
-
+// Deze functie word opgeroepen wanneer de pagina is geladen
 function init() {
+  // Hier sorteert hij de burgers van klein naar groot
   burgers.sort(function(a, b) {
     if (a.price > b.price) return +1;
     if (a.price < b.price) return -1;
     return 0;
   })
+  // Hier selecteer ik de menu en voor elke element in de array maakt hij de burger aan
   const menu = document.querySelector('.menu');
   burgers.forEach(element => {
     let item = document.createElement('li');
     item.innerHTML =  `<img alt='img' src='img/${element.picture}'>` +
                       `<p class='name'>${element.name}</p>` +
                       `<p class='price'>${element.price}</p>`;
+    // Hier zet hij de item in de menu
     menu.append(item);
   });
 }
